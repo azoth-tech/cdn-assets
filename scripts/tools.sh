78d927bccd8d8601e88721e94a45ab4568489498
azoth-tech <dvpandian@gmail.com>
2026-01-24 18:48:35 +0400

    Update tools.sh

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -5,7 +5,7 @@ pulumi-cleanup() { npm run pulumi:cleanup -- --stack $1 --auto --remove-stack; }
 pulumi-setup() { npm run pulumi:setup -- --properties "../$1" --auto; }
 
 gitacp() {
-    git add -A && git commit -m "$1" && git push
+    git add -A && git commit -m "$1" && git push && git status --short
 }
 
 
