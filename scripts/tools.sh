bd3db9e4ac35622328ed016ce5be1844826ed253
azoth-tech <dvpandian@gmail.com>
2026-01-27 01:22:24 +0400

    Add pulumi-trash function for cleanup
    Add pulumi-trash function to clean up Pulumi instances and configuration files.

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -4,6 +4,13 @@ pulumi-cleanup() { npm run pulumi:cleanup -- --stack $1 --auto --remove-stack; }
 
 pulumi-setup() { npm run pulumi:setup -- --properties "../$1" --auto; }
 
+ pulumi-trash{
+  rm -rf .instances
+  rm -rf infrastructure/.pulumi; rm infrastructure/Pulumi.*.yaml
+}
+
+
+
 gitacp() {
     git add -A && git commit -m "$*" && git push && git --no-pager diff --name-status HEAD~1
 }
